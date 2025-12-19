b1003b3f8a264c1c3b475e27f919a82f289a54d0
moesato0319 <niku.imo.ramen.curry.312@gmail.com>
2025-12-19 15:38:37 +0900

    修正: バリデーション/ログアウト/投稿エラー対応

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -34,7 +34,7 @@ class RegisteredUserController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'name' => ['required', 'string', 'max:255','regex:/\S/'], //←空白のみ禁止
+            'name' => ['required', 'string', 'max:255','regex:/^(?!\s*$).+/'], //←全角・半角スペースのみ禁止
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
